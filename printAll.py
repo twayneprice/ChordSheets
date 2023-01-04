0aa87b43741ab399809a61169f355c4c40ed2404
Wayne Price <twayneprice@gmail.com>
2023-01-04 13:25:48 -0600

    new songs

--- printAll.py
+++ printAll.py
@@ -6,12 +6,11 @@ from operator import itemgetter
 
 def printAll(inUrl):
     files = getFileList(inUrl)
-    files = files#[5:6]
+    files = files[0:1]
     for fileDict in files:
-        # print(fileDict)
-
         name = fileDict['name'].split('.')[0]
         file = fileDict['download_url']
+        print(name)
 
         dir = './prints/orig/'
         cmd = 'google-chrome --headless --disable-gpu --print-to-pdf="'+dir+name+'.pdf" "file:///home/wayne/Downloads/ChordSheets/onSongViewer.html?file='+file+'"'
@@ -31,25 +30,12 @@ def printAll(inUrl):
         # print(cmd)
         os.system(cmd)
 
-
-        # break
-
-
 def getFileList(inUrl):
     data = {}
     with urllib.request.urlopen(inUrl) as url:
         data = json.load(url)
     
     return sorted(data, key=itemgetter('name'))
-
-
-# google-chrome --headless --disable-gpu --print-to-pdf=out.pdf "file:///home/wayne/Downloads/ChordSheets/onSongViewer.html?fontAdjust=12&transpose=2"
-
-# google-chrome --headless --disable-gpu --print-to-pdf=out.pdf "file:///home/wayne/Downloads/ChordSheets/onSongViewer.html?file=https://twayneprice.github.io/ChordSheets/onSong/10,000%20Reasons.onsong&transpose=4"
-
-
-# https://twayneprice.github.io/ChordSheets/songs.html?v=0.6646505201266948
-
 
 if __name__ == "__main__":
     printAll("https://api.github.com/repos/twayneprice/ChordSheets/contents/onSong")
