cefa331ec509e45aaa075945864527e08a8ca9a6
Wayne Price <twayneprice@gmail.com>
2023-01-16 10:03:28 -0600

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,3 +1,8 @@
-convert -pointsize 20 -draw "text 400,20 '$(date -dnext-sunday +%m-%d-%Y)  Wayne Sells'" "./prints/orig/Forever Reign.png" "./prints/orig/*Fly Away.png" "./prints/orig/Man Of Sorrows.png" "./prints/orig/*Dry Bones.png" -page letter ./prints/$(date -dnext-sunday +%m-%d-%Y).pdf
+file1="Forever Reign"
+file2="*Fly Away"
+file3="Man Of Sorrows"
+file4="*Dry Bones"
 
-convert -pointsize 20 -draw "text 400,20 '$(date -dnext-sunday +%m-%d-%Y)'" "./prints/capo/Forever Reign.png" "./prints/capo/*Fly Away.png" "./prints/capo/Man Of Sorrows.png" "./prints/capo/*Dry Bones.png" -page letter ./prints/$(date -dnext-sunday +%m-%d-%Y)-Capo.pdf
+convert -pointsize 20 -draw "text 400,20 '$(date -dnext-sunday +%m-%d-%Y)  Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" -page letter ./prints/$(date -dnext-sunday +%m-%d-%Y).pdf
+
+convert -pointsize 20 -draw "text 400,20 '$(date -dnext-sunday +%m-%d-%Y)'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" -page letter ./prints/$(date -dnext-sunday +%m-%d-%Y)-Capo.pdf
