5574d69044069b76e8db9277ac1ca3671a5c4d7d
Wayne Price <twayneprice@gmail.com>
2023-01-18 15:28:36 -0600

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,7 +1,7 @@
-file1="Forever Reign"
-file2="*Fly Away"
-file3="Man Of Sorrows"
-file4="*Dry Bones"
+file1="Less Like Me"
+file2="When the Roll*"
+file3="Blessed Redeemer"
+file4="Holy Water"
 
 convert -pointsize 20 -draw "text 400,20 '$(date -dnext-sunday +%m-%d-%Y)  Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" -page letter ./prints/$(date -dnext-sunday +%m-%d-%Y).pdf
 
