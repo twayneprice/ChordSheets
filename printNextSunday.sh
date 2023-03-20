e039c92de93f614e9dc0fe920765ab7606090815
Wayne Price <twayneprice@gmail.com>
2023-03-20 10:44:44 -0500

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,7 +1,11 @@
-file1="Victory In Jesus"
-file2="Me On Your Mind"
-file3="I Am Not Alone"
-file4="Man Of Sorrows"
+file1="*Passion*"
+file2="The Old Rugged Cross"
+file3="Man Of Sorrows"
+file4="King Of Kings"
+# file1="Less Like Me"
+# file2="*My Chains*"
+# file3="Well Done"
+# file4="You Say"
 
 
 # convert -page letter+0-10 -pointsize 15 -draw "text 150,2 '$(date -dnext-sunday +%m-%d-%Y)  Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" -resize 600x800 -gravity North -format pdf ./$(date -dnext-sunday +%m-%d-%Y).pdf
