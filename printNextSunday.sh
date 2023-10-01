55881bb0a6eb3a1e9ee6c57a5bc83691bf0d129e
Wayne Price <twayneprice@gmail.com>
2023-09-30 20:10:19 -0500

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,12 +1,13 @@
-file1="This Is Amazing Grace"
-file2="Me On Your Mind"
-file3="*Dry Bones*"
-file4="Well Done"
+file1="He Set Me Free"
+file2="Scars In Heaven"
+file3="Glorious Day"
+file4="Living Hope"
+file5="Youre Still God"
 
 
-convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" -format pdf ./Sunday.pdf
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" "./prints/orig/${file5}.png" "./prints/orig/${file6}.png" -format pdf ./Sunday.pdf
 
-convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Band'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" -format pdf ./Sunday-Capo.pdf
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Band'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" "./prints/capo/${file5}.png" "./prints/capo/${file6}.png" -format pdf ./Sunday-Capo.pdf
 
 
 
