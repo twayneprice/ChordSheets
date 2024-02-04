678ad49fd3f53d97bda0a629984e43b9f1bd667a
Wayne Price <twayneprice@gmail.com>
2024-02-03 19:25:07 -0600

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,9 +1,9 @@
-file1="This Is Our God"
-file2="The Old Rugged Cross"
-file3="Scars In Heaven"
-file4="Blessed Redeemer"
+file1="Holy Water"
+file2="What A Friend We Have In Jesus"
+file3="Less Like Me"
+file4="Fires"
 file5="Unclouded Day"
-file6="Fires"
+file6=""
 
 
 convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" "./prints/orig/${file5}.png" "./prints/orig/${file6}.png" -format pdf ./Sunday.pdf
