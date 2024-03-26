7b02f63aca44c611f4952028092610fe4f9a93d7
Wayne Price <twayneprice@gmail.com>
2024-03-26 13:39:44 -0500

    new songs

--- printNextSunday.sh
+++ printNextSunday.sh
@@ -1,12 +1,14 @@
-file1="Youre Still God"
-file2="Ill Fly Away"
-file3="Goodness Of God"
-file4="I Lift My Hands"
-file5="Unclouded Day"
+file1="This Is Amazing Grace"
+file2="Because He Lives"
+file3="Rise Again"
+file4=""
+file5=""
+file6=""
 file6=""
 
 
-convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" "./prints/orig/${file5}.png" "./prints/orig/${file6}.png" -format pdf ./Sunday.pdf
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" "./prints/orig/${file5}.png" "./prints/orig/${file6}.png" "./prints/orig/${file7}.png" -format pdf ./Sunday.pdf
 
-convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Band'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" "./prints/capo/${file5}.png" "./prints/capo/${file6}.png" -format pdf ./Sunday-Capo.pdf
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Band'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" "./prints/capo/${file5}.png" "./prints/capo/${file6}.png" "./prints/capo/${file7}.png" -format pdf ./Sunday-Capo.pdf
 
+cd /home/wayne//Documents//Chords/ChordSheets && ./gitPushAll.sh
