4b3934837e091d3b97fee48fcdb4ac3e2100c49e
Wayne Price <twayneprice@gmail.com>
2024-05-13 13:00:05 -0500

    new songs

--- gitPushAll.sh
+++ gitPushAll.sh
@@ -1,6 +1,6 @@
 git add -A
 git commit -m "new songs"
-git push
+git push -u https://twayneprice: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@github.com/twayneprice/ChordSheets.git main
 sleep 30
 
 python3 printAll.py
@@ -9,4 +9,4 @@ python3 printAll.py
 
 git add -A
 git commit -m "new songs"
-git push
+git push -u https://twayneprice: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@github.com/twayneprice/ChordSheets.git main
