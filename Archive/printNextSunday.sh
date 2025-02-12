86b71251bca5953a37cfe950d3f832a4f3544adc
Wayne Price <twayneprice@gmail.com>
2025-02-12 11:49:59 -0600

    new songs

---
+++ Archive/printNextSunday.sh
@@ -0,0 +1,16 @@
+file1="Me And God"
+file2="The Old Rugged Cross"
+file3="No Longer Slaves"
+file4="Scars"
+file5="Survivor"
+file6="God So Loved"
+file7=""
+
+
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Wayne Sells'" "./prints/orig/${file1}.png" "./prints/orig/${file2}.png" "./prints/orig/${file3}.png" "./prints/orig/${file4}.png" "./prints/orig/${file5}.png" "./prints/orig/${file6}.png" "./prints/orig/${file7}.png" -format pdf ./Sunday.pdf
+
+convert -page letter+0-10 -pointsize 15 -draw "text 150,2 'Band'" "./prints/capo/${file1}.png" "./prints/capo/${file2}.png" "./prints/capo/${file3}.png" "./prints/capo/${file4}.png" "./prints/capo/${file5}.png" "./prints/capo/${file6}.png" "./prints/capo/${file7}.png" -format pdf ./Sunday-Capo.pdf
+
+# cd /home/wayne//Documents//Chords/ChordSheets && ./gitPushAll.sh
+
+
