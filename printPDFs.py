b08dd25d5c179071bbee1a8910db60842253b9e6
Wayne Price <twayneprice@gmail.com>
2025-03-29 00:00:01 -0500

    new songs

--- printPDFs.py
+++ printPDFs.py
@@ -9,10 +9,10 @@ from PyPDF2 import PdfMerger
 
 # List of Sunday songs in the desired order.
 sunday_songs = [
-    "Fires",
-    "Great Is Thy Faithfulness",
-    "My Jesus",
-    "Youre Still God",
+    "There Was Jesus",
+    "What A Friend We Have In Jesus",
+    "Trust In God",
+    "Lord I Need You",
     "Survivor",
     "God So Loved"
 ]
