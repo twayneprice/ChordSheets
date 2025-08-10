7cf67a1d98fea92107e2f77f5faaf0ba2f2724f7
Wayne Price <twayneprice@gmail.com>
2025-08-09 19:04:24 -0500

    new songs

--- printPDFs.py
+++ printPDFs.py
@@ -9,10 +9,11 @@ from PyPDF2 import PdfMerger
 
 # List of Sunday songs in the desired order.
 sunday_songs = [
-    "On My Knees",
-    "Leave It There",
-    "Me On Your Mind",
-    "O Come To The Alter"
+    "He Set Me Free",
+    "Jesus Fault",
+    "There Was Jesus",
+    "The Truth",
+    "Im A Winner Either Way"
 ]
 
 # ----- Argument Parsing -----
